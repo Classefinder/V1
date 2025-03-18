9946480945e729aebb3cfe23cee3a14fa438f5db
Sauroyn <sauroynitro@gmail.com>
2025-03-18 13:42:25 +0100

    test

--- webview_app/lib/main.dart
+++ webview_app/lib/main.dart
@@ -8,6 +8,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,6 +21,8 @@ class MyApp extends StatelessWidget {
 }
 
 class WebViewScreen extends StatefulWidget {
+  const WebViewScreen({super.key});
+
   @override
   _WebViewScreenState createState() => _WebViewScreenState();
 }
